73a35e78e04f328a2ef4b818ca756cfd6028591a
JuvenMi <canercincin34@gmail.com>
2023-03-31 03:29:39 +0300

    first load

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    //file dosyasının bulunmasında sıkıntı olabilir, kesin yolu kopyalarak sorunun üstesinden geldim.
+    static File file = new File("C:\\Users\\Caner Çin\\OneDrive\\Masaüstü\\java\\untitled2\\src\\text.txt");
+
+    public static void main(String[] args) throws FileNotFoundException {
+        NodeOperations operations = new NodeOperations();
+        Scanner reader = new Scanner(file);
+        int nodeCount = nodeCounter(), column = 0;
+        Node[] nodeHeads = new Node[nodeCount];
+        while (reader.hasNextLine()){
+            String line = reader.nextLine();
+            String[] list = line.split("->|/");
+            for (int item = 0; item < list.length; item = item + 2){
+                if(item == 0){
+                    nodeHeads[column] = new Node(list[item], Integer.parseInt(list[item + 1]));
+                }else {
+                    nodeHeads[column] = NodeOperations.addNode(nodeHeads[column], new Node(list[item], Integer.parseInt(list[item + 1])));
+                }
+            }
+            column++;
+        }
+        operations.adjacencyListPrinter(nodeHeads); //test passed
+        System.out.println(operations.entryDegreeCounter(nodeHeads,42)); //try of all
+        System.out.println(operations.outputDegreeCounter(nodeHeads,42));
+        System.out.println(operations.edgeCounter(nodeHeads));
+        operations.whereToGo(nodeHeads, 34);
+        operations.cameCityFrom(nodeHeads, 34);
+    }
+
+    /**
+     * @return Returns the number of nodes in the graph.
+     */
+    public static int nodeCounter() throws FileNotFoundException {
+        Scanner reader = new Scanner(file);
+        int nodeCounter = 0;
+        while (reader.hasNextLine()){
+            reader.nextLine();
+            nodeCounter++;
+        }
+        return nodeCounter;
+    }
+}
---
+++ src/NodeOperations.java
@@ -0,0 +1,160 @@
+public class NodeOperations {
+    /**
+     * @param headNode linked list's first node
+     * @param willAddNode node to add to linked list
+     * @return headNode
+     */
+    public static Node addNode(Node headNode, Node willAddNode){
+        if (headNode == null){
+            headNode = willAddNode;
+        }else {
+            Node tempHead = headNode;
+            while (tempHead.nextNode != null){
+                tempHead = tempHead.nextNode;
+            }
+            tempHead.nextNode = willAddNode;
+        }
+        return headNode;
+    }
+
+    /**
+     * @param nodeHeads adjacency list of nodes in a graph
+     */
+    public void adjacencyListPrinter(Node[] nodeHeads){
+        for (Node node: nodeHeads) {
+            while(node != null){
+                if (node.nextNode != null){
+                    System.out.printf("%s/%d -> ", node.cityName, node.cityNum);
+                }else {
+                    System.out.printf("%s/%d", node.cityName, node.cityNum);
+                }
+                node = node.nextNode;
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * @param nodeHeads adjacency list of nodes in a graph
+     * @param searchingNodeNum entry degree is the number of the search node
+     * @return entry degree of related node
+     */
+    public int entryDegreeCounter(Node[] nodeHeads, int searchingNodeNum){
+        int entryDegreeCount = 0;
+        for (Node node : nodeHeads) {
+            node = node.nextNode;
+            while (node != null){
+                if (node.cityNum == searchingNodeNum){
+                    entryDegreeCount++;
+                    break;
+                }else {
+                    node = node.nextNode;
+                }
+            }
+        }
+        return entryDegreeCount;
+    }
+
+    /**
+     * @param nodeHeads adjacency list of nodes in a graph
+     * @param searchingNodeNum output degree is the number of the search node
+     * @return output degree of related node
+     */
+    public int outputDegreeCounter(Node[] nodeHeads, int searchingNodeNum){
+        int outputDegreeCount = 0;
+        for (Node node : nodeHeads){
+            if (node.cityNum == searchingNodeNum){
+                node = node.nextNode;
+                while (node != null){
+                    outputDegreeCount++;
+                    node = node.nextNode;
+                }
+            }
+        }
+        return outputDegreeCount;
+    }
+
+    /**
+     * @param nodeHeads adjacency list of nodes in a graph
+     * @return edge count of graph
+     */
+    public int edgeCounter(Node[] nodeHeads){
+        int edgeCount = 0;
+        for (Node node : nodeHeads){
+            while (node != null){
+                edgeCount++;
+                node = node.nextNode;
+            }
+            edgeCount--;
+        }
+        return edgeCount;
+    }
+
+    /**
+     * @param nodeHeads adjacency list of nodes in a graph
+     * @param fromWhichCity departure city number
+     */
+    public void whereToGo(Node[] nodeHeads, int fromWhichCity){
+        for (Node node : nodeHeads){
+            if (node.cityNum == fromWhichCity){
+                System.out.printf("%s/%d şehrinden; ", node.cityName, node.cityNum);
+                node = node.nextNode;
+                while(node != null){
+                    System.out.printf(" %s/%d", node.cityName, node.cityNum);
+                    node = node.nextNode;
+                    System.out.print(",");
+                }
+                System.out.println(" gidilir.");
+                break;
+            }
+        }
+    }
+
+    /**
+     * @param nodeHeads adjacency list of nodes in a graph
+     * @param fromWhereCity arrive city number
+     */
+    public void cameCityFrom(Node[] nodeHeads, int fromWhereCity){
+        Node fromWhere = whichCity(nodeHeads,fromWhereCity);
+        Node listHead = null;
+        for (Node node : nodeHeads){
+            Node rowHead = node;
+            node = node.nextNode;
+            while(node != null){
+                if (node.cityNum == fromWhereCity){
+                    listHead = addNode(listHead, new Node(rowHead.cityName, rowHead.cityNum));
+                }
+                node = node.nextNode;
+            }
+        }
+        assert fromWhere != null;
+        ccfPrinter(listHead, fromWhere);
+    }
+
+    /**
+     * @param nodeHeads adjacency list of nodes in a graph
+     * @param cityNum the number of the searched city
+     * @return the searched city
+     */
+    public static Node whichCity(Node[] nodeHeads,int cityNum){
+        for (Node node : nodeHeads){
+            if (node.cityNum == cityNum){
+                return node;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param listHead linked list of departure city for came city
+     * @param city came of which city
+     */
+    public static void ccfPrinter(Node listHead, Node city){
+        System.out.printf("%s/%d şehrine;", city.cityName, city.cityNum);
+        while (listHead != null){
+            System.out.printf(" %s/%d,", listHead.cityName, listHead.cityNum);
+            listHead = listHead.nextNode;
+        }
+        System.out.print("gelinebilir.");
+    }
+}
